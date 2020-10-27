be11b0902749eaee891af3638f3f3e29fab7d225
Alois Janíček <janicek.dev@gmail.com>
2020-10-27 12:00:08 +0100

    allow to set custom network vis options
    introducing `org-roam-server-light-network-vis-options` elisp variable
    which can be used to store JSON string containing additional options
    to be send by python server to vis.Network when graph is created
    
    A lot of features can be turn on or off and a lot of settings can be
    adjusted, but from my experience, defaults are just fine
    
    see
    https://visjs.github.io/vis-network/docs/network/
    for an overview of all available options

--- response/networkVisHandler.py
+++ response/networkVisHandler.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
+import os.path
 from response.requestHandler import RequestHandler
+from variables import org_roam_server_light_tmp_dir
+
+
+network_vis_options_file = (
+    org_roam_server_light_tmp_dir
+    /
+    "org-roam-server-light-network-vis-options"
+)
 
 
 class NetworkVisHandler(RequestHandler):
     def __init__(self):
         super().__init__()
+        global network_vis_options_file
         self.contentType = "application/json"
-        self.contents = "{}"
+
+        if os.path.isfile(network_vis_options_file) and os.path.getsize(network_vis_options_file) > 0:
+            network_vis_options = open(
+                network_vis_options_file, "r").read()
+            print('match: \n')
+            print(network_vis_options)
+            print('\n')
+            self.contents = network_vis_options
+            print(self.contents)
+        else:
+            self.contents = "{}"
+            print('this MUST NOT never happen: \n')
+
         self.setStatus(200)
 
     def getContents(self):
